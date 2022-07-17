44e29e40dad66f933e6fa1e7b8dedfcf399d9a5c
Peter Arato <it.arato@gmail.com>
2022-07-17 16:03:50 -0400

    Fix tab bug

--- split_unit.h
+++ split_unit.h
@@ -20,7 +20,8 @@ struct SplitUnit {
 
   void newTextView() {
     textViews.emplace_back(textViewCols(), textViewRows());
-    activeTextViewIdx = textViews.size() - 1;
+    setActiveTextViewIdx(textViews.size() - 1);
+    updateDimensions(cols, rows);
   }
 
   void setActiveTextViewIdx(int newValue) {
@@ -40,38 +41,45 @@ struct SplitUnit {
   }
 
   void generateTextViewsTabsLine(string& out) {
+    string tabsLine{};
+
     int maxTitleSize = cols / textViews.size();
 
-    out.append("\x1b[7m\x1b[90m");
+    tabsLine.append("\x1b[7m\x1b[90m");
 
     if (maxTitleSize < 5) {
-      out.append("\x1b[39m ");
-      out.append(
+      tabsLine.append("\x1b[39m ");
+      tabsLine.append(
           activeTextView()->fileName().value_or("<no file>").substr(0, cols));
     } else {
       for (int i = 0; i < (int)textViews.size(); i++) {
         if (i == activeTextViewIdx) {
-          out.append("\x1b[39m ");
+          tabsLine.append("\x1b[39m ");
         } else {
-          out.append("\x1b[90m ");
+          tabsLine.append("\x1b[90m ");
         }
         // TODO only use filename part
-        out.append(textViews[i]
-                       .fileName()
-                       .value_or("<no file>")
-                       .substr(0, maxTitleSize - 3));
+        tabsLine.append(textViews[i]
+                            .fileName()
+                            .value_or("<no file>")
+                            .substr(0, maxTitleSize - 3));
 
         if (i < (int)textViews.size() - 1) {
-          out.append(" \x1b[90m:");
+          tabsLine.append(" \x1b[90m:");
         } else {
-          out.append(" \x1b[90m");
+          tabsLine.append(" \x1b[90m");
         }
       }
     }
 
-    string suffix(cols - visibleCharCount(out), ' ');
-    out.append(suffix);
-    out.append("\x1b[0m");
+    int paddingSpacesLen = cols - visibleCharCount(tabsLine);
+    if (paddingSpacesLen > 0) {
+      string suffix(paddingSpacesLen, ' ');
+      tabsLine.append(suffix);
+    }
+    tabsLine.append("\x1b[0m");
+
+    out.append(tabsLine);
   }
 
   inline int textViewCols() const { return cols; }
