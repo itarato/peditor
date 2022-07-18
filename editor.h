fddb497df4373ed711e24fdc0a62751fceed1dd8
Peter Arato <it.arato@gmail.com>
2022-07-18 14:28:03 -0400

    tidy

--- editor.h
+++ editor.h
@@ -20,6 +20,7 @@
 #include "config.h"
 #include "debug.h"
 #include "file_watcher.h"
+#include "prompt.h"
 #include "split_unit.h"
 #include "terminal_util.h"
 #include "text_manipulator.h"
@@ -31,60 +32,6 @@ using namespace std;
 enum class EditorMode {
   TextEdit,
   Prompt,
-};
-
-enum class PromptCommand {
-  Nothing,
-  SaveFileAs,
-  OpenFile,
-  MultiPurpose,
-  FileHasBeenModified,
-};
-
-struct Prompt {
-  string prefix{};
-  PromptCommand command{PromptCommand::Nothing};
-  string rawMessage{};
-
-  bool isAutoCompleteOn{false};
-  vector<string> messageOptions{};
-
-  void reset(string newPrefix, PromptCommand newCommand) {
-    prefix = newPrefix;
-    rawMessage.clear();
-    command = newCommand;
-    isAutoCompleteOn = false;
-  }
-
-  void reset(string newPrefix, PromptCommand newCommand,
-             vector<string>&& newMessageOptions) {
-    reset(newPrefix, newCommand);
-
-    isAutoCompleteOn = true;
-    messageOptions = move(newMessageOptions);
-  }
-
-  string message(bool withHighlights = false) {
-    if (isAutoCompleteOn) {
-      auto filePaths = directoryFiles();
-      auto matches = poormansFuzzySearch(rawMessage, filePaths, 1);
-
-      if (matches.empty()) {
-        return rawMessage;
-      } else if (withHighlights) {
-        return highlightPoormanFuzzyMatch(rawMessage, matches[0]);
-      } else {
-        return matches[0];
-      }
-    } else {
-      return rawMessage;
-    }
-  }
-
-  inline int messageVisibleSize() {
-    string s{message()};
-    return visibleCharCount(s);
-  }
 };
 
 struct Editor {
