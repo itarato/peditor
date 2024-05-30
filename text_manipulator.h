4525705c67a6a4636d9d6ba792ca1f5d1f562c95
Peter Arato <it.arato@gmail.com>
2024-05-29 20:48:59 -0400

    integrate

--- text_manipulator.h
+++ text_manipulator.h
@@ -23,19 +23,9 @@ void execute(Command *cmd, Lines &lines) {
   } else if (cmd->type == CommandType::DeleteSlice) {
     lines[cmd->row].erase(cmd->col, cmd->memoryStr.size());
   } else if (cmd->type == CommandType::SplitLine) {
-    auto rowIt = lines->at(cmd->row).begin();
-    advance(rowIt, cmd->col);
-    string newLine(rowIt, lines->at(cmd->row).end());
-
-    auto rowIt2 = lines->at(cmd->row).begin();
-    advance(rowIt2, cmd->col);
-    lines->at(cmd->row).erase(rowIt2, lines->at(cmd->row).end());
-
-    auto lineIt = lines->begin();
-    advance(lineIt, cmd->row + 1);
-    lines->insert(lineIt, newLine);
+    lines.insert(cmd->row, cmd->col, "\n");
   } else if (cmd->type == CommandType::InsertSlice) {
-    lines->at(cmd->row).insert(cmd->col, cmd->memoryStr);
+    lines.insert(cmd->row, cmd->col, cmd->memoryStr);
   } else if (cmd->type == CommandType::SwapLine) {
     auto lineIt = lines->begin();
     advance(lineIt, cmd->row);
